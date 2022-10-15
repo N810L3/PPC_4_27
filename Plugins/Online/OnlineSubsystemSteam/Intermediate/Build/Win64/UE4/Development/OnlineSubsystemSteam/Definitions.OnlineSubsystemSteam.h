7fa54ceff1932da3dd1680a86ae5ddd78e4f4f9b
Nathan Le <nle935726@gmail.com>
2022-10-15 11:57:12 -0700

    quality of life updates to BP

--- Plugins/Online/OnlineSubsystemSteam/Intermediate/Build/Win64/UE4/Development/OnlineSubsystemSteam/Definitions.OnlineSubsystemSteam.h
+++ Plugins/Online/OnlineSubsystemSteam/Intermediate/Build/Win64/UE4/Development/OnlineSubsystemSteam/Definitions.OnlineSubsystemSteam.h
@@ -35,7 +35,7 @@
 #define WITH_CEF3 1
 #define WITH_LIVE_CODING 1
 #define UE_LIVE_CODING_ENGINE_DIR "C:\\Program Files\\Epic Games\\UE_4.27\\Engine"
-#define UE_LIVE_CODING_PROJECT "C:\\Users\\nle93\\Documents\\Unreal Projects\\PPC_MP_4_27\\PPC_MP_4_27.uproject"
+#define UE_LIVE_CODING_PROJECT "C:\\Users\\nle93\\Desktop\\PPC_Repo\\PPC_4_27\\PPC_MP_4_27.uproject"
 #define UBT_MODULE_MANIFEST "UE4.modules"
 #define UBT_MODULE_MANIFEST_DEBUGGAME "UE4-Win64-DebugGame.modules"
 #define UBT_COMPILED_PLATFORM Win64
